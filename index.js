93c361992b520500f826666391e87a16b1c29795
Tony Gorez <gorez.tony@gmail.com>
2026-02-12 15:56:58 +0100

    feat: expose rotate
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- index.js
+++ index.js
@@ -2,25 +2,60 @@ const Log = require('bare-logger')
 const fs = require('bare-fs')
 
 module.exports = class FileLog extends Log {
+  #path
+  #maxSize
+  #rotate
+  #rotateInterval
+  #interval = null
+  #fd
+
   constructor(path, opts = {}) {
-    const { maxSize = 0 } = opts
+    const { maxSize = 0, rotate = null, rotateInterval = 2000 } = opts
 
     super({ colors: false })
 
-    this._path = path
-    this._maxSize = maxSize
-    this._fd = fs.openSync(this._path, 'a+')
+    this.#path = path
+    this.#maxSize = maxSize
+    this.#rotate = rotate
+    this.#rotateInterval = rotateInterval
+    this.#fd = fs.openSync(this.#path, 'a+')
 
-    if (this._maxSize > 0 && fs.fstatSync(this._fd).size > this._maxSize) {
+    if (this.#maxSize > 0 && fs.fstatSync(this.#fd).size > this.#maxSize) {
       this.clear()
     }
   }
 
+  #checkRotate() {
+    if (this.#fd === -1) return
+    if (this.#maxSize <= 0 || this.#rotate === null) return
+
+    const size = fs.fstatSync(this.#fd).size
+
+    if (size >= this.#maxSize * 0.8) {
+      const dest = this.#rotate(this.#path)
+
+      if (dest) {
+        fs.closeSync(this.#fd)
+        fs.renameSync(this.#path, dest)
+        this.#fd = fs.openSync(this.#path, 'a+')
+      }
+    }
+  }
+
+  #startRotateCheck() {
+    if (this.#interval !== null) return
+    if (this.#maxSize <= 0 || this.#rotate === null) return
+
+    this.#interval = setInterval(() => this.#checkRotate(), this.#rotateInterval)
+  }
+
   append(label, ...data) {
     fs.writeSync(
-      this._fd,
+      this.#fd,
       label.padEnd(5, ' ') + ' ' + new Date().toISOString() + ' ' + this.format(...data) + '\n'
     )
+
+    this.#startRotateCheck()
   }
 
   debug(...data) {
@@ -44,15 +79,19 @@ module.exports = class FileLog extends Log {
   }
 
   clear() {
-    fs.ftruncateSync(this._fd)
-    fs.closeSync(this._fd)
-    this._fd = fs.openSync(this._path, 'a+')
+    fs.ftruncateSync(this.#fd)
+    fs.closeSync(this.#fd)
+    this.#fd = fs.openSync(this.#path, 'a+')
   }
 
   close() {
-    if (this._fd === -1) return
-    fs.closeSync(this._fd)
-    this._fd = -1
+    if (this.#fd === -1) return
+    if (this.#interval !== null) {
+      clearInterval(this.#interval)
+      this.#interval = null
+    }
+    fs.closeSync(this.#fd)
+    this.#fd = -1
   }
 
   [Symbol.dispose]() {
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const test = require('brittle')
 const path = require('bare-path')
+const fs = require('bare-fs')
+
 const FileLog = require('.')
 
 test('basic', async (t) => {
@@ -10,4 +12,84 @@ test('basic', async (t) => {
   await t.execution(() => log.info('This is an info log'))
   await t.execution(() => log.warn('This is a warning log'))
   await t.execution(() => log.error('This is an error log'))
+})
+
+test('rotate - renames file when 80% of maxSize is reached', async (t) => {
+  t.plan(4)
+
+  const tmp = await t.tmp()
+  const logPath = path.join(tmp, 'test.log')
+  const rotatedPath = path.join(tmp, 'test.log.1')
+
+  const log = new FileLog(logPath, {
+    maxSize: 50,
+    rotateInterval: 100,
+    rotate(filePath) {
+      t.is(filePath, logPath)
+      return rotatedPath
+    }
+  })
+  t.teardown(() => log.close())
+
+  for (let i = 0; i < 5; i++) {
+    log.info('filling up the log file with data')
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 200))
+
+  t.ok(fs.existsSync(rotatedPath), 'rotated file exists')
+  t.ok(fs.statSync(rotatedPath).size > 0, 'rotated file has content')
+  t.is(fs.statSync(logPath).size, 0, 'new log file is empty')
+})
+
+test('rotate - does nothing when rotate returns falsy', async (t) => {
+  t.plan(2)
+
+  const tmp = await t.tmp()
+  const logPath = path.join(tmp, 'test.log')
+
+  let sizeBefore = 0
+
+  const log = new FileLog(logPath, {
+    maxSize: 50,
+    rotateInterval: 100,
+    rotate() {
+      sizeBefore = fs.statSync(logPath).size
+      t.pass('rotate was called')
+      log.close()
+      return null
+    }
+  })
+  t.teardown(() => log.close())
+
+  for (let i = 0; i < 5; i++) {
+    log.info('filling up the log file with data')
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 200))
+
+  t.is(fs.statSync(logPath).size, sizeBefore, 'file was not modified')
+})
+
+test('rotate - does not call rotate when under 80% threshold', async (t) => {
+  t.plan(1)
+
+  const tmp = await t.tmp()
+  const logPath = path.join(tmp, 'test.log')
+
+  const log = new FileLog(logPath, {
+    maxSize: 100000,
+    rotateInterval: 100,
+    rotate() {
+      t.fail('rotate should not be called')
+      return path.join(tmp, 'test.log.1')
+    }
+  })
+  t.teardown(() => log.close())
+
+  log.info('short')
+
+  await new Promise((resolve) => setTimeout(resolve, 200))
+
+  t.pass('rotate was not called')
 })
