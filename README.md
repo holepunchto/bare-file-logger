8bc35a073365c1d1445a5b122a33fa0d24fe1124
Tony Gorez <gorez.tony@gmail.com>
2026-02-13 04:54:09 -0500

    feat: add rotate api (#2)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- README.md
+++ README.md
@@ -23,9 +23,37 @@ options = {
   // Maximum allowed byte size of the log file. This is a hint and not a hard
   // limit; the logger will do its best to keep the file size within the limit,
   // but provides no guarantees.
-  maxSize: 0
+  maxSize: 0,
+
+  // A function called when the file size reaches 80% of `maxSize`. It receives
+  // the current file path and should return a new path to rename the file to,
+  // or a falsy value to do nothing. After a successful rename, a new empty log
+  // file is opened at the original path.
+  rotate: null,
+
+  // The interval in milliseconds at which the file size is checked against
+  // `maxSize` for rotation. Only active when both `maxSize` and `rotate` are
+  // set. The check starts after the first write.
+  rotateInterval: 2000
 }
 ```
+
+#### Rotation
+
+```js
+const log = new FileLog('app.log', {
+  maxSize: 1024 * 1024, // 1 MB
+  rotate(filePath) {
+    return filePath + '.' + Date.now()
+  }
+})
+```
+
+When the log file reaches 80% of `maxSize`, the `rotate` function is called. If it returns a path, the current file is renamed to that path and a fresh log file is opened. If it returns a falsy value, no action is taken.
+
+#### `log.on('rotate', (path, dest) => {})`
+
+Emitted after a successful rotation. `path` is the original log file path (now empty and ready for new writes) and `dest` is the path the previous log was renamed to.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const Log = require('bare-logger')
+const EventEmitter = require('bare-events')
 const fs = require('bare-fs')
 
-module.exports = class FileLog extends Log {
+class FileLog extends EventEmitter {
   constructor(path, opts = {}) {
-    const { maxSize = 0 } = opts
+    const { maxSize = 0, rotate = null, rotateInterval = 2000 } = opts
 
-    super({ colors: false })
+    super()
 
+    this.colors = false
     this._path = path
     this._maxSize = maxSize
+    this._rotate = rotate
+    this._rotateInterval = rotateInterval
+    this._interval = null
     this._fd = fs.openSync(this._path, 'a+')
 
     if (this._maxSize > 0 && fs.fstatSync(this._fd).size > this._maxSize) {
@@ -16,11 +21,39 @@ module.exports = class FileLog extends Log {
     }
   }
 
+  _checkRotate() {
+    if (this._fd === -1) return
+    if (this._maxSize <= 0 || this._rotate === null) return
+
+    const size = fs.fstatSync(this._fd).size
+
+    if (size >= this._maxSize) {
+      const dest = this._rotate(this._path)
+
+      if (dest) {
+        fs.closeSync(this._fd)
+        fs.renameSync(this._path, dest)
+        this._fd = fs.openSync(this._path, 'a+')
+        this.emit('rotate', this._path, dest)
+      }
+    }
+  }
+
+  _startRotateCheck() {
+    if (this._interval !== null) return
+    if (this._maxSize <= 0 || this._rotate === null) return
+
+    this._interval = setInterval(() => this._checkRotate(), this._rotateInterval)
+    this._interval.unref()
+  }
+
   append(label, ...data) {
     fs.writeSync(
       this._fd,
       label.padEnd(5, ' ') + ' ' + new Date().toISOString() + ' ' + this.format(...data) + '\n'
     )
+
+    this._startRotateCheck()
   }
 
   debug(...data) {
@@ -51,6 +84,10 @@ module.exports = class FileLog extends Log {
 
   close() {
     if (this._fd === -1) return
+    if (this._interval !== null) {
+      clearInterval(this._interval)
+      this._interval = null
+    }
     fs.closeSync(this._fd)
     this._fd = -1
   }
@@ -59,3 +96,7 @@ module.exports = class FileLog extends Log {
     this.close()
   }
 }
+
+FileLog.prototype.format = Log.prototype.format
+
+module.exports = FileLog
